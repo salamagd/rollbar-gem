442e9ddf89a950cca1a6200c43cc01815df1d778
Jon de Andres <jon@rollbar.com>
2016-03-29 16:01:26 +0200

    tmp

--- lib/rollbar/request_data_extractor.rb
+++ lib/rollbar/request_data_extractor.rb
@@ -1,13 +1,13 @@
 require 'rack'
 require 'tempfile'
 
+require 'rollbar/scrubbers'
 require 'rollbar/scrubbers/url'
+require 'rollbar/scrubbers/params'
 require 'rollbar/util/ip_obfuscator'
 
 module Rollbar
   module RequestDataExtractor
-    SKIPPED_CLASSES = [Tempfile]
-
     def extract_person_data_from_controller(env)
       if env.has_key? 'rollbar.person_data'
         person_data = env['rollbar.person_data'] || {}
@@ -57,14 +57,6 @@ module Rollbar
       data
     end
 
-    def rollbar_scrubbed(value)
-      if Rollbar.configuration.randomize_scrub_length
-        random_filtered_value
-      else
-        '*' * (value.length rescue 8)
-      end
-    end
-
     private
 
     def mergeable_raw_body_params(rack_req)
@@ -89,7 +81,7 @@ module Rollbar
         if name == 'Cookie'
           {}
         elsif sensitive_headers_list.include?(name)
-          { name => rollbar_scrubbed(env[header]) }
+          { name => Rollbar::Scrubbers.scrub_value(env[header]) }
         else
           { name => env[header] }
         end
@@ -189,59 +181,15 @@ module Rollbar
     end
 
     def rollbar_filtered_params(sensitive_params, params)
-      sensitive_params_regexp = Regexp.new(sensitive_params.map{ |val| Regexp.escape(val.to_s).to_s }.join('|'), true)
-
-      return {} unless params
-
-      params.to_hash.inject({}) do |result, (key, value)|
-        if sensitive_params_regexp =~ Rollbar::Encoding.encode(key).to_s
-          result[key] = rollbar_scrubbed(value)
-        elsif value.is_a?(Hash)
-          result[key] = rollbar_filtered_params(sensitive_params, value)
-        elsif value.is_a?(Array)
-          result[key] = value.map do |v|
-            v.is_a?(Hash) ? rollbar_filtered_params(sensitive_params, v) : rollbar_filtered_param_value(v)
-          end
-        elsif skip_value?(value)
-          result[key] = "Skipped value of class '#{value.class.name}'"
-        else
-          result[key] = rollbar_filtered_param_value(value)
-        end
-
-        result
-      end
-    end
-
-    def rollbar_filtered_param_value(value)
-      if ATTACHMENT_CLASSES.include?(value.class.name)
-        begin
-          {
-            :content_type => value.content_type,
-            :original_filename => value.original_filename,
-            :size => value.tempfile.size
-          }
-        rescue
-          'Uploaded file'
-        end
-      else
-        value
-      end
+      Rollbar::Scrubbers::Params.call(params, sensitive_params)
     end
 
     def sensitive_params_list(env)
-      Array(Rollbar.configuration.scrub_fields) | Array(env['action_dispatch.parameter_filter'])
+      Array(env['action_dispatch.parameter_filter'])
     end
 
     def sensitive_headers_list
       Rollbar.configuration.scrub_headers || []
-    end
-
-    def random_filtered_value
-      '*' * (rand(5) + 3)
-    end
-
-    def skip_value?(value)
-      SKIPPED_CLASSES.any? { |klass| value.is_a?(klass) }
     end
   end
 end
